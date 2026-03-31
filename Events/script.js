09aa178d3d5c08971b8d5aa59a4f39d01b96150b
Priyanshu <priyanshuguptawebdev@gmail.com>
2026-03-31 20:30:46 +0530

    Done a Homework Problem

--- Events/script.js
+++ Events/script.js
@@ -1,12 +1,19 @@
 
 let btn = document.querySelector("#toggle-btn");
 let body = document.querySelector("body");
+let para = document.querySelector("#hidden");
 
-handler = ()=>{
+handler1 = ()=>{
     if(body.style.backgroundColor == "black") {
         body.style.backgroundColor = "white";
     } else {
         body.style.backgroundColor = "black";
     }
 }
-btn.addEventListener("click", handler);
+
+handler2 = () => {
+    para.style.visibility = "visible";
+}
+
+btn.addEventListener("click", handler1);
+btn.addEventListener("mouseover", handler2)
